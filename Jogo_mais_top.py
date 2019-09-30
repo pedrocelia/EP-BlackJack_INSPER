ef0c29f26625ecb56ab943c9c0b5dc34c8b6721f
Pedro Celia <pedrodc1@al.insper.edu.br>
2019-09-30 11:00:48 -0300

    inicio do codigo

---
+++ Jogo_mais_top.py
@@ -0,0 +1,59 @@
+# Black Jack Mais top do mundo.
+
+dinheiro = 150
+
+resposta = input('Vai querer apostar: ')
+JOGO = True
+
+pontos_jogador = 0
+
+baralho = {2:2,
+           3:3,
+           4:4,
+           5:5,
+           6:6,
+           7:7,
+           8:8,
+           9:9,
+           10:10,
+           'J':10,
+           'Q':10,
+           'K':10,
+           'A':[1,11]}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#while resposta != 'não' or resposta != 'nao':
+#    aposta = float(int('quanto vai apostar? '))
+#    while aposta != 0:
+#        carta1 = random.randint(1,14)
+#        carta2 = random.randint(1,14)
+#        print('sua carta é: {0}' .format(carta))
+#        pontos_jogador += carta1 + carta2
+#        jogada = input( 'o que você vai fazer? ')
+#        if jogada == 'carta'
+#    
+
