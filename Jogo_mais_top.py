aac42b9891a9f9d7b41df7f5b98b5c283bd706dd
Pedro Celia <pedrodc1@al.insper.edu.br>
2019-09-30 11:49:05 -0300

    baralho feito

--- Jogo_mais_top.py
+++ Jogo_mais_top.py
@@ -1,13 +1,6 @@
 # Black Jack Mais top do mundo.
 
-dinheiro = 150
-
-resposta = input('Vai querer apostar: ')
-JOGO = True
-
-pontos_jogador = 0
-
-baralho = {2:2,
+baralho_valores = {2:2,
            3:3,
            4:4,
            5:5,
@@ -21,39 +14,36 @@ baralho = {2:2,
            'K':10,
            'A':[1,11]}
 
+baralho_quant = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K','A','A','A','A']
+
+nome=input('qual é o seu nome? ')
+
+print('---------------------------------------------------------')
+print('Bem vinda(o), {0}, você está prestes a jogar o melhor blackjack de todos os tempos!!!!!' .format(nome))
+print('---------------------------------------------------------')
+print('Esse é o BlackJack FOX, se prepare para a melhor experiencia de todas!\n'
+      'O BlackJack FOX é um blackjack de casino, onde temos multiplas features '
+      'para você se divertir e ganhar dinheiro ao máximo!')
+print('---------------------------------------------------------')
+print('REGRAS (basicas):\n'
+      'O jogo é jogado com um (ou mais baralhos de 52 cartas). Para isso as cartas têm a seguinte pontuação:\n'
+      '•Cartas com números,valem o valor impresso nelas (cartas de 2 a 10)\n'
+      '•Cartas de faces (Valete, Dama, Rei) valem 10 pontos.\n'
+      '•O Ás pode valer tanto 1 ou 11 pontosO computador deverá fazer o papel do croupier.')
+
+
+print('---------------------------------------------------------')
+init = input('vamos começar?')
+print('Você vai começar com 150 reais')
+print('---------------------------------------------------------')
+
+quant_baralhos = int(input('quantos baralhos você vai querer usar? '))
+deck = baralho_quant*quant_baralhos
+
+JOGO = True
+dinheiro = 150
+pontos_jogador = 0
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-#while resposta != 'não' or resposta != 'nao':
-#    aposta = float(int('quanto vai apostar? '))
-#    while aposta != 0:
-#        carta1 = random.randint(1,14)
-#        carta2 = random.randint(1,14)
-#        print('sua carta é: {0}' .format(carta))
-#        pontos_jogador += carta1 + carta2
-#        jogada = input( 'o que você vai fazer? ')
-#        if jogada == 'carta'
-#    
 
