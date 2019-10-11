d6fac91c83a2a7cbc34514f9864d4b2ed4634160
Gabriela Giosa <gabichoichit@gmail.com>
2019-10-11 10:46:47 -0300

    Iniciei o loop principal

--- Jogo_mais_top.py
+++ Jogo_mais_top.py
@@ -46,104 +46,111 @@ deck = baralho_quant*quant_baralhos
 
 JOGO = True
 
-aposta = int(input("Quanto deseja apostar?"))
-dinheiro = 150 - aposta
-pontos_jogador = 0
-pontos_crupier = 0
 
-
-
-
-
-
-
-
-
+while JOGO:
+    aposta = int(input("Quanto deseja apostar?"))
+    dinheiro = 150 - aposta
+    pontos_jogador = 0
+    pontos_crupier = 0
     
-cartas = random.sample(deck,2)
-deck.remove(cartas[0])
-deck.remove(cartas[1])
-
-cru_car = random.sample(deck,2)
-deck.remove(cru_car[0])
-
-print('Essa é a carta do crupier {0}' .format(cru_car[0]))
-
-   
-print('Essas são suas cartas {0}' .format(cartas))
-
     
-for e in cartas:
-    if e in baralho_valores:
-        if e == 'A':
-            if pontos_jogador + 11 > 21:
-                pontos_jogador+=baralho_valores[e[0]]
-            else:
-                pontos_jogador += baralho_valores[e[1]]
-        pontos_jogador += baralho_valores[e]    
-print('Isso é quantidade de pontos que você tem: {0}' .format(pontos_jogador))
-
-
-for c in cru_car:
-    if c in baralho_valores:
-        if e == 'A':
-            if pontos_crupier + 11 > 21:
-                    pontos_crupier+=baralho_valores[c[0]]
-            else:
-                pontos_crupier += baralho_valores[c[1]]
-        pontos_crupier += baralho_valores[c]
-
-
-while pontos_jogador < 21:
-    opcao = input('Suas opções são:    PARAR    ou   CARTA, escolha: ')
-    if opcao == "CARTA":
-        cartas = random.sample(deck,1)
-        print("Essa é a sua carta{0}".format(cartas))
-        for e in cartas:
-            if e in baralho_valores:
-                pontos_jogador += baralho_valores[e]
-                print("Essa é a sua pontuação agora: ", pontos_jogador)
-
-# não altera o dinheiro                
-if pontos_jogador > 21 and pontos_crupier > 21:
-    print("Ninguém ganha nada")
-    dinheiro += aposta
- 
-# não altera o dinheiro
-elif pontos_jogador == 21 and pontos_crupier == 21:
-    print("Ninguém ganha nada")
-    dinheiro += aposta
     
-# Dinheiro = 2X o valor apostado     
-elif pontos_jogador < 21 and pontos_crupier > 21:
-    print("Parabéns, você ganhou!!")
-    dinheiro += 2 * aposta
     
-#Dinheiro = - aposta
-elif pontos_crupier < 21 and pontos_jogador > 21:
-    print("Você perdeu")
-    dinheiro = dinheiro
     
-# Dinheiro = 2.5X aposta
-elif pontos_jogador == 21 and pontos_crupier != 21:
-    print("BLACKJACK!!")
-    dinheiro += 2.5 * aposta
-
-# Dinheiro = -2.5 x aposta    
-elif pontos_crupier == 21 and pontos_jogador != 21:
-    print("BLACKJACK do crupier!!")
-    dinheiro -= 1.5 * aposta
-
-# Dinheiro = 2 x aposta
-elif pontos_jogador > pontos_crupier:
-    print("Você ganhou!")
-    dinheiro += 2 * aposta
-
-# Dinheiro = -2 x aposta    
-else:
-    print("Você perdeu")
-    dinheiro = dinheiro
- 
+    
+    
+    
+    
+        
+    cartas = random.sample(deck,2)
+    deck.remove(cartas[0])
+    deck.remove(cartas[1])
+    
+    cru_car = random.sample(deck,2)
+    deck.remove(cru_car[0])
+    
+    print('Essa é a carta do crupier {0}' .format(cru_car[0]))
+    
+       
+    print('Essas são suas cartas {0}' .format(cartas))
+    
+        
+    for e in cartas:
+        if e in baralho_valores:
+            if e == 'A':
+                if pontos_jogador + 11 > 21:
+                    pontos_jogador+=baralho_valores[e[0]]
+                else:
+                    pontos_jogador += baralho_valores[e[1]]
+            pontos_jogador += baralho_valores[e]    
+    print('Isso é quantidade de pontos que você tem: {0}' .format(pontos_jogador))
+    
+    
+    for c in cru_car:
+        if c in baralho_valores:
+            if e == 'A':
+                if pontos_crupier + 11 > 21:
+                        pontos_crupier+=baralho_valores[c[0]]
+                else:
+                    pontos_crupier += baralho_valores[c[1]]
+            pontos_crupier += baralho_valores[c]
+    
+    
+    while pontos_jogador < 21:
+        opcao = input('Suas opções são:    PARAR    ou   CARTA, escolha: ')
+        if opcao == "CARTA":
+            cartas = random.sample(deck,1)
+            print("Essa é a sua carta{0}".format(cartas))
+            for e in cartas:
+                if e in baralho_valores:
+                    pontos_jogador += baralho_valores[e]
+                    print("Essa é a sua pontuação agora: ", pontos_jogador)
+        if opcao == "PARAR":
+            break
+    
+    # não altera o dinheiro                
+    if pontos_jogador > 21 and pontos_crupier > 21:
+        print("Ninguém ganha nada")
+        dinheiro += aposta
+     
+    # não altera o dinheiro
+    elif pontos_jogador == 21 and pontos_crupier == 21:
+        print("Ninguém ganha nada")
+        dinheiro += aposta
+        
+    # Dinheiro = 2X o valor apostado     
+    elif pontos_jogador < 21 and pontos_crupier > 21:
+        print("Parabéns, você ganhou!!")
+        dinheiro += 2 * aposta
+        
+    #Dinheiro = - aposta
+    elif pontos_crupier < 21 and pontos_jogador > 21:
+        print("Você perdeu")
+        dinheiro = dinheiro
+        
+    # Dinheiro = 2.5X aposta
+    elif pontos_jogador == 21 and pontos_crupier != 21:
+        print("BLACKJACK!!")
+        dinheiro += 2.5 * aposta
+    
+    # Dinheiro = -2.5 x aposta    
+    elif pontos_crupier == 21 and pontos_jogador != 21:
+        print("BLACKJACK do crupier!!")
+        dinheiro -= 1.5 * aposta
+    
+    # Dinheiro = 2 x aposta
+    elif pontos_jogador > pontos_crupier:
+        print("Você ganhou!")
+        dinheiro += 2 * aposta
+    
+    # Dinheiro = -2 x aposta    
+    else:
+        print("Você perdeu")
+        dinheiro = dinheiro
+    
+    
+    if dinheiro == 0:
+        JOGO = False
 
 
 
